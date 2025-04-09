7e8df0548d565ddd14ee90612f4660295ac13dae
matthewexe <matteobaldi233@gmail.com>
2025-04-09 19:06:38 +0200

    utils default

---
+++ src/utils/default.py
@@ -0,0 +1,27 @@
+from random import random
+from model.spin import DataSPIN
+
+
+class Defaults:
+
+    @staticmethod
+    def choiceChild(net: DataSPIN, p_id: str):
+        prop = net.get_props(p_id)
+        for p, c in prop.distribution:
+            if p == 1:
+                return c
+
+    @staticmethod
+    def natureChild(net: DataSPIN, p_id: str):
+        choice = random()
+        sum_p = 0
+        prop = net.get_props(p_id)
+        last = None
+
+        for p, c in prop.distribution:
+            sum_p += p
+            if sum_p >= choice:
+                return c
+            last = c
+
+        return last
