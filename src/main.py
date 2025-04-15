2f54c0849bc0c1513536cfe3562597c94340e8d7
matthewexe <matteobaldi233@gmail.com>
2025-04-15 17:36:06 +0200

    checkout

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Request, status, Response
 from fastapi.responses import RedirectResponse, HTMLResponse
-from converter.spin import create_pnml_from_region
-from converter.test_bpmn import region_to_bpmn, Region, _region_info
+from converter.spin import create_pnml_from_region, getProps
 from pm4py.objects.petri_net.importer.variants.pnml import import_net_from_string
 from pm4py.objects.conversion.bpmn.variants import to_petri_net
 from pm4py.objects.conversion.bpmn.variants.to_petri_net import Parameters
 from pm4py.visualization.petri_net.common.visualize import graphviz_visualization
 
 from model.region import RegionModel
+from model.spin import DataSPIN
 
 api = FastAPI()
 
@@ -25,18 +25,26 @@ def root(request: Request):
     return RedirectResponse("/docs/", status_code=status.HTTP_303_SEE_OTHER)
 
 
-@api.post("/test")
+@api.post("/step")
+def step():
+    return ""
+
+
+@api.post("/start")
 def test(data: RegionModel):
     pnml = create_pnml_from_region(data)
-    with open("iron.pnml", "w+") as f:
+    with open("tests/iron.pnml", "w+") as f:
         f.write(pnml)
 
-    petri_net, im, fm = import_net_from_string(pnml)
+    raw_petri_net, im, fm = import_net_from_string(pnml)
+    props, d_match = getProps()
+    spin = DataSPIN(raw_petri_net, props, d_match)
+
     graph_obj = graphviz_visualization(
-        petri_net, "png", initial_marking=im, final_marking=fm
+        raw_petri_net, "png", initial_marking=im, final_marking=fm
     )
     graph_obj.save("tests/iron_petri.dot")
 
     # return _region_info
-    return Response(content=str(petri_net), media_type="text/plain")
+    return Response(content=str(raw_petri_net), media_type="text/plain")
     # return Response(content=region_to_bpmn(data), media_type="application/xml")
