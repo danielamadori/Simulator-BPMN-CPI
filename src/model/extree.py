c1f4b8667176e18c8264410e8e5d98788690de81
matthewexe <matteobaldi233@gmail.com>
2025-05-22 18:35:51 +0200

    in order add in execution tree

--- src/model/extree.py
+++ src/model/extree.py
@@ -1,15 +1,18 @@
-import uuid
+import itertools
+import logging
 
 from anytree import Node, PreOrderIter, RenderTree
 
 from utils.net_utils import NetUtils
+from .context import NetContext
 from .snapshot import Snapshot
-import logging
+from .types import T
 
 logger = logging.getLogger(__name__)
 
 
 class ExTree:
+    __separator = '/'
     current_node: Node
     __root: Node
 
@@ -20,7 +23,7 @@ class ExTree:
             logger.error("Lo Snapshot Root è None")
             raise ValueError("Root Snapshot can't be None")
 
-        _root = Node(name="Root", id=str(uuid.uuid4()), snapshot=root)
+        _root = Node(name="Root", id='-1', snapshot=root)
         self.__root = _root
         self.current_node = _root
 
@@ -38,12 +41,6 @@ class ExTree:
         impacts = [0] * len(place_impacts)
         return ExTree(Snapshot(ctx.initial_marking, 1, impacts, 0))
 
-    def find_by_id(self, node_id: str):
-        for node in PreOrderIter(self.__root):
-            if node._id == node_id:
-                return node
-        return None
-
     def get_nodes(self):
         return list(PreOrderIter(self.__root))
 
@@ -51,15 +48,16 @@ class ExTree:
         return node in self
 
     # Costruzione dell'albero
-    def add_snapshot(self, snapshot: Snapshot, set_as_current: bool = True):
+    def add_snapshot(self, ctx, snapshot: Snapshot, set_as_current: bool = True):
         parent = self.current_node
         for node in self:
             if node.parent == parent and node.snapshot.marking == snapshot.marking:
                 return node
 
-        uuid_ = uuid.uuid4()
+        idx = add_exec_inorder(ctx, self, snapshot)
+        _id = parent.name + ExTree.__separator + idx
         child_node = Node(
-            name=str(uuid_), id=str(uuid_), snapshot=snapshot, parent=parent
+            name=str(_id), id=str(_id), snapshot=snapshot, parent=parent
         )
 
         if set_as_current:
@@ -113,3 +111,40 @@ def is_equal(node1: Node, node2: Node):
         return False
 
     return True
+
+
+def add_exec_inorder(ctx, tree: ExTree, new_snapshot):
+    sorted_child = get_current_sorted_child(ctx, tree.current_node.snapshot.marking)
+    idx_child = sorted_child.index(new_snapshot.marking)
+    tree.add_snapshot(new_snapshot)
+
+    return idx_child
+
+
+def get_current_sorted_child(ctx: NetContext, marking) -> list[T]:
+    net = ctx.net
+    curr_time_marking = marking
+    exec_strategy = ctx.strategy
+
+    choices = exec_strategy.get_choices(net, curr_time_marking)
+    m = [choices[choice] for choice in choices]
+    perms = itertools.product(*m)
+    key_to_time_marking = {}
+
+    for perm in perms:
+        temp, *_ = exec_strategy.consume(net, curr_time_marking, perm)
+        key_of_time_marking = []
+        for k in sorted(temp.keys()):
+            token, age = temp[k]
+            if token == 0:
+                key_of_time_marking.append(-1)
+            else:
+                key_of_time_marking.append(age)
+
+        key_to_time_marking.update({tuple(key_of_time_marking): temp})
+
+    children = []
+    for key_row in sorted(list(key_to_time_marking.keys())):
+        children.append(key_to_time_marking[key_row])
+
+    return children
