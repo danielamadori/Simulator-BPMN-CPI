9aa92b7afc8fbc4f83b8cb3154bd67a53de6fe41
matthewexe <matteobaldi233@gmail.com>
2025-09-23 04:22:53 +0200

    feat(settings): change env alias to avoid possible naming collisions

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -1,14 +1,14 @@
 #  Copyright (c) 2025.
-
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class APISettings(BaseSettings):
-    host: str = "127.0.0.1"
-    port: int = 8001
-    title: str = "BPMN-CPI Simulator API"
-    version: str = "1.0.0"
-    docs_url: str = "/docs/"
+    host: str = Field(alias="simulator_api_host", default="127.0.0.1")
+    port: int = Field(alias="simulator_api_port", default=8001)
+    title: str = Field(alias="simulator_api_title", default="BPMN-CPI Simulator API")
+    version: str = Field(alias="simulator_api_version", default="1.0.0")
+    docs_url: str = Field(alias="simulator_api_docs_url", default="/docs/")
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
